23bcb86f2b0cdb10f1e86fdda4bfafa16b1da211
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-07-28 17:59:48 +0100

    Parse project intros as markdown

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
+import markdownIt from 'markdown-it'
 import PageMetadata from './components/PageMetadata.js'
+
+const md = markdownIt({
+  html: true,
+  breaks: true,
+  linkify: true
+})
 
 export default function (eleventyConfig) {
   eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
 
+  eleventyConfig.addFilter("markdown", function(value) {
+    return md.render(value)
+  });
   eleventyConfig.addShortcode("PageMetadata", PageMetadata);
 
   return {
