9ef28221f52ccb883024b555dd16c5408407bc45
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-07-28 18:43:07 +0100

    Add subject count to subject sets

--- site/projects/index.11tydata.js
+++ site/projects/index.11tydata.js
@@ -4,7 +4,8 @@ function titlePages({ project, subjects, subjectSets }) {
     if (subject) {
       return {
         href: `/transcription-explorer/subjects/${subject.id}`,
-        title: subjectSet.display_name
+        title: subjectSet.display_name,
+        subjectCount: subjectSet.set_member_subjects_count
       }
     }
     return
