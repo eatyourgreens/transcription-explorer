e36d9dc89954f288ccb4ef322237a0fa58bceafe
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-07-31 16:31:36 +0100

    Request subjects per project (#3)

--- site/_data/subjects.js
+++ site/_data/subjects.js
@@ -1,7 +1,5 @@
-import fetchWithRetry from './fetchWithRetry.js'
+import fetchWithRetry from "./fetchWithRetry.js";
 import config from "./config.js";
-
-const projectIDs = config.projects.join(",");
 
 /*
 Fetches ALL Subjects from a Project.
@@ -10,19 +8,19 @@ Ouput:
 (array of objects) array of Panoptes Subject resources 
  */
 async function fetchAllSubjects(query) {
-  let allSubjects = []
-  let continueFetching = true
-  let page = 1
+  let allSubjects = [];
+  let continueFetching = true;
+  let page = 1;
 
   while (continueFetching) {
-    const { subjects, meta } = await fetchSubjectsByPage(query, page)
-    allSubjects = allSubjects.concat(subjects)
-    continueFetching = (+meta.page <= +meta.page_count) || false
-    page++
+    const { subjects, meta } = await fetchSubjectsByPage(query, page);
+    allSubjects = allSubjects.concat(subjects);
+    continueFetching = +meta.page <= +meta.page_count || false;
+    page++;
   }
 
-  const uniqueSubjects = [...new Set(allSubjects)]
-  console.log('subjects:', uniqueSubjects.length)
+  const uniqueSubjects = [...new Set(allSubjects)];
+  console.log("subjects:", uniqueSubjects.length);
   return uniqueSubjects.sort((a, b) => parseInt(a.id) - parseInt(b.id));
 }
 
@@ -36,19 +34,23 @@ Output: (object) {
  */
 async function fetchSubjectsByPage(query, page = 1, pageSize = 100) {
   try {
-    const { subjects, meta }  = await fetchWithRetry('/subjects', {
+    const { subjects, meta } = await fetchWithRetry("/subjects", {
       ...query,
       page,
       page_size: pageSize,
-      order: 'id'
-    })
-    return { subjects, meta: meta.subjects }
+      order: "id",
+    });
+    return { subjects, meta: meta.subjects };
   } catch (err) {
-    console.error('ERROR: fetchSubjectsByPage()')
-    console.error('- error: ', err)
-    console.error('- args: ', projectId, page, pageSize)
-    throw(err)
+    console.error("ERROR: fetchSubjectsByPage()");
+    console.error("- error: ", err);
+    console.error("- args: ", projectId, page, pageSize);
+    throw err;
   }
 }
-
-export default await fetchAllSubjects({ project_id: projectIDs })
+const subjects = await Promise.all(
+  config.projects.map(
+    async (projectID) => await fetchAllSubjects({ project_id: projectID })
+  )
+);
+export default subjects.flat();
