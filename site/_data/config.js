8dac5900e16fdc07190ec06d745165979d32bb7f
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-10-28 08:47:11 +0000

    feat: Documentation Detectives

--- site/_data/config.js
+++ site/_data/config.js
@@ -1,14 +1,15 @@
 const siteRoot = process.env.SITE_ROOT || '';
 const projects = [
-    17426,
-    15597,
-    15969,
-    15591,
-    14978,
-    12856,
-    9006,
-    5481,
-    4973
+    21982, // Documentation Detectives
+    17426, // PRINT
+    15597, // Poets & Lovers
+    15969, // Maria Edgeworth Letters
+    15591, // People's Contest Digital Archive
+    14978, // Corresponding With Quakers
+    12856, // Beyond Borders
+    9006, // Davy Notebooks
+    5481, // Deciphering Secrets
+    4973 // Anti-Slavery Manuscripts
 ];
 export default {
     siteRoot,
